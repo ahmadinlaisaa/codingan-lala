7052a5228c31ddfc5aeded7c267c0e9c49fe5c95
ahmadin laisa <ahmadinlaisa03@gmail.com>
2026-02-24 16:10:53 +0700

    about bar &  search bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,19 +15,42 @@
     </head>
     <body class="antialiased">
         <div class="bg-slate-300 h-screen">
-            <div class="about-bar">
-                <div class="about-bar-menu">
-                    <ul>
-                        <li>PERSONAL</li>
+
+            {{-- about bar --}}
+            <div class="about-bar flex justify-between bg-red-50 text-sm text-[#131749]">
+                <div class="about-bar-menu px-10 py-4">
+                    <ul class="flex justify-between space-x-5">
+                        <li class="font-[670] text-red-500">PERSONAL</li>
                         <li>ENTERPRISE</li>
                         <li>TENTANG KAMI</li>
                     </ul>
                 </div>
-                <div class="about-bar-menu">
-                    <ul>
+                <div class="about-bar-menu px-10 py-4">
+                    <ul class="flex justify-between space-x-5">
                         <li>TSHOP</li>
                         <li>MYTELKOMSEL</li>
-                        <li>EN | ID</li>
+                        {{-- <div>
+                            <span>ID</span>
+                            <span class="mx-2">|</span>
+                            <span>EN</span>
+                        </div> --}}
+                    </ul>
+                </div>
+            </div>
+            {{-- logo navbar --}}
+            <div class="about-bar flex justify-between bg-red-50 text-sm text-[#131749] border-b-[3px] border-gray-400">
+                <div class="about-bar-menu px-5 py-3">
+                    <img src="{{ asset('img/logo-telkomsel.png') }}" alt="Telkomsel" class="w-33">
+                </div>
+                <div class="about-bar-menu px-5 py-3 bg-red-50 flex items-center">
+                    <ul class="flex items-center justify-between space-x-5">
+                        <li>*LOGO SEARCH</li>
+                        {{-- <li>*HAMBURGER UNTUK MOBILE</li> --}}
+                        {{-- <div>
+                            <span>ID</span>
+                            <span class="mx-2">|</span>
+                            <span>EN</span>
+                        </div> --}}
                     </ul>
                 </div>
             </div>
